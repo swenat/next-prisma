e5e5f1c6754897ab6254c8110bfc5e164684915c
Nathalie Lysefeldt <lysefeldt@outlook.com>
2024-06-12 13:31:05 +0200

    added get for h1

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,6 @@
 describe("template spec", () => {
 	it("passes", () => {
 		cy.visit("/"); //vi ser till att den går till localhost men kom ihåg att utvecklingsservern måste vara igång för att få det  att gå igenom för tillfället
+		cy.get("h1").contains("Welcome to my Blog").should("be.visible");
 	});
 });
