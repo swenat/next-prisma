23fdeb6cdc70e055070a60dd630066f5cc1f9704
Nathalie Lysefeldt <lysefeldt@outlook.com>
2024-06-13 12:34:23 +0200

    added Lysefeldt to cypresstest and also added header on site

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -5,7 +5,7 @@ describe("template spec", () => {
 	it("passes", () => {
 		cy.visit("/"); //vi ser till att den går till localhost men kom ihåg att utvecklingsservern måste vara igång för att få det  att gå igenom för tillfället
 		cy.get("header").should("exist");
-		cy.get("header h1").contains("The Gallery").should("be.visible");
+		cy.get("header h1").contains("The Lysefeldt Gallery").should("be.visible");
 
 		cy.get("h1").contains("Welcome to my Blog").should("be.visible");
 	});
